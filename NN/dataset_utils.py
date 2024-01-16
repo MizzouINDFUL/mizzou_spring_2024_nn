058a6be81d6d03033d82e5ee9cb0a893b9b4919f
DerekThomasAnderson <42683677+DerekThomasAnderson@users.noreply.github.com>
2024-01-16 10:26:49 -0600

    Pushing NN folder
    Push it

---
+++ NN/dataset_utils.py
@@ -0,0 +1,22 @@
+import torch                                                                                # Library: Pytorch 
+import torch.utils.data as utils                                                            # Library: Pytorch Dataset
+
+class Format_Dataset(utils.Dataset):
+
+    def __init__(self, data_params, choice):
+        
+        self.choice = choice 
+        self.samples = torch.Tensor(data_params['samples']).to(torch.float64)             # Gather: Data Samples
+        if(self.choice.lower() == 'train'): 
+            self.labels = torch.Tensor(data_params['labels']).to(torch.float64)           # Gather: Data Labels
+        
+    def __getitem__(self, index):                                                           
+        
+        if(self.choice.lower() == 'train'): 
+            return self.samples[index], self.labels[index]                                  # Return: Next (Sample, Label) 
+        else:
+            return self.samples[index]                                                     
+
+    def __len__(self):                                                                      
+        
+        return len(self.samples)
